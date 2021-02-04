266c7bf788df7c462303d9f0e4e61bf49f0f21d3
Adam Mill <hismajesty@theroyalwhee.com>
2021-02-03 21:52:29 -0500

    Add initial jsdoc support.

--- src/istype.js
+++ src/istype.js
@@ -1,11 +1,17 @@
 /**
- * @theroyalwhee0/snowman:src/istype.js
- *
- * TODO: Move to @theroyalwhee0/istype
+ * @file Is-Type utilities.
+ * @author Adam Mill <hismajesty@theroyalwhee.com>
+ * @copyright Copyright 2021 Adam MillS
+ * @license Apache-2.0
  */
+
+// TODO: Move to @theroyalwhee0/istype
 
 /**
  * Is the type a BigInt.
+ * @param {any} value The value to check type of.
+ * @returns {boolean} Is value a bigint?
+ * @private
  */
 function isBigInt(value) {
   return typeof value === 'bigint';
